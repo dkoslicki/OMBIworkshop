6326da0c426fd10d5fd255bda77d2b358b302bf8
dkoslicki <david.koslicki@math.oregonstate.edu>
2017-05-11 14:36:59 -0700

    added data files

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -9,3 +9,6 @@ beta_diversity_through_plots.py -i input/otu_table.biom -o results/bdiv -m input
 # note the -e setting (set the depth of coverage for even sampling)
 # to specify a different kind of distance metric, -p specifies a parameter file (so you can
 # adjust the underlying distance metric in the sub-scripts in this workflow)
+
+# Statistical
+compare_categories.py -i results/bdiv/weighted_unifrac_dm.txt --method adonis -m input/map.txt -o adonis_results -c growth_rate,complex_robust,host_genus_id,functional_group_sensu_darling
